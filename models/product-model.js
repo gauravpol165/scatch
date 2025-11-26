1719fdea4c75d5d972f47480eafa78e364dcd607
gauravpol165 <gauravpol819@gmail.com>
2025-11-26 19:46:14 +0530

    26/11 ch work ahe

---
+++ models/product-model.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose')
+
+// mongoose.connect("mongoodb://127.0.0.1:27017/scatch");
+
+const productSchema=mongoose.Schema({
+    image:String,
+    name:String,
+    price:Number,
+    discount:{
+        type:Number,
+        default:0,
+    },
+    bgcolor:String,
+    panelcolor:String,
+    textcolor:String,
+});
+
+module.exports=mongoose.model("product",productSchema);
